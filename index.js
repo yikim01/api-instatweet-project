a8a20964446878dc6f36ad359146c3afad13c5b6
Kimberly Yi <yikim01@gmail.com>
2016-08-10 23:11:18 -0700

    update: instagram api success

--- index.js
+++ index.js
@@ -2,9 +2,17 @@ var express = require('express');
 var logger = require('morgan');
 var app = express();
 var path = require('path');
+var pg = require('pg');
 
 app.use(logger('dev'));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(express.static('public'));
+
+
 
 app.listen(process.env.PORT || 3000, function () {
   console.log('Listening on http://localhost:' + (process.env.PORT || 3000))
---
+++ public/js/script.js
@@ -0,0 +1,32 @@
+
+  // var request = new XMLHttpRequest();
+
+  // request.onload = function() {
+  //   if (request.status === 200) {
+  //     // Success!!
+  //     var resp = request.responseText
+  //     console.log('Success!' + resp);
+  //   }
+  // };
+
+  // request.onerror = function() {
+  //   console.log("server error");
+  // };
+
+  // request.open('GET', 'https://api.instagram.com/v1/users/self/media/recent/?access_token=3256527.1677ed0.cdbc6ce2681742fab477b432f0f333ac&callback=events', true);
+  // request.send();
+
+function showEvents (data) {
+  var newContent = '';
+//build string with called content
+  for (var i = 0; i < data.data.length; i++) {
+    newContent += '<div class="events">';
+    newContent += '<img src="' + data.data[i].images.low_resolution.url + '"';
+    newContent += ' alt="' + data.data[i].caption.text + '" />';
+    newContent += '<p><b>' + data.data[i].caption.text + '</b></br>';
+    newContent += '</div>';
+  }
+//update page with content
+document.getElementById('content').innerHTML = newContent;
+}
+
